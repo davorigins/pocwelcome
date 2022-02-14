61febe73f23fd411ead72b069a2719bab0f303f2
Stephane Lavaud <stephane.lavaud@origins-digital.com>
2022-02-14 11:21:12 +0100

    feat(caprover): add caprover settings

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
+app.post("/", function (req, res) {
+  console.log(JSON.stringify(req.body, 0, 2));
+  res.status(200).send(req.body);
+});
 
-app.post("/", function(req, res) {  
-   console.log(JSON.stringify(req.body, 0, 2)); 
-   res.status(200).send(req.body);
-  })
-
-
-
-app.listen(3000, function () {
-  console.log('WELCOME POC listening on port 3000!');
+app.listen(8000, function () {
+  console.log("WELCOME POC listening on port 3000!");
 });
